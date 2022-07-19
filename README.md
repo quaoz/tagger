f488790daee3a23b111494491c8398984450f320
quaoz <74419801+quaoz@users.noreply.github.com>
2022-07-19 20:37:51 +0100

    hot

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# tagger
+# Tagger
+
+A script to tag scrobbled albums on [last.fm](https://last.fm) with genres (and optionally descriptors) from [Rate Your Music](https://rateyourmusic.com). The script is slow mainly due to the long time taken to retrieve tags from RYM, to tag 1000 albums took me around 6 hours. Excessive scraping of RYM may result in you being IP banned for a few days however this is not a problem I have run into.
+
+## Installation
+
+```shell
+# Clone this repository
+git clone https://github.com/quaoz/tagger.git
+cd tagger
+
+# Rename example.env to .env and fill in your details
+cp example.env .env
+nano .env
+
+# Clone rymscraper
+git clone https://github.com/dbeley/rymscraper.git
+
+# Create the pipenv and install the requirements
+pipenv shell
+pipenv install -e rymscraper
+pipenv install -r requirements.txt
+
+# Check that the options defined in the script suite you. At the very least you will need to change DRY_RUN to False for the script to do anything but you should read through all the options.
+# The options begin after line 98 (if __name__ == '__main__':)
+nano main.py
+
+# Run the script
+python main.py
+```
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import re
 import time
@@ -7,12 +8,89 @@ from dotenv import load_dotenv
 from rymscraper import rymscraper
 
 
-def clean(items):
+def get_tags(album_item: pylast.Album, clean_name: bool) -> list[str]:
+	message(album_item, i, album_count, clean_name=clean_name)
+	album_title = clean_album_title(album_item.title) if clean_name else album_item.title
+
+	# Retrieve the album from RYM
+	rym_album = rym_network.get_album_infos(name=f"{album_item.artist} - {album_title}")
+
+	# Extract and process the genres and descriptors
+	genres = clean_tags(rym_album.get("Genres").replace("\n", ", ").split(", "))
+	if PRINT_TAGS:
+		print(f"Genres:      {genres}")
+
+	if USE_DESCRIPTORS:
+		descriptors = clean_tags(rym_album.get("Descriptors").replace("\n", ", ").split(", "))
+		if PRINT_TAGS:
+			print(f"Descriptors: {descriptors}")
+		genres = genres + descriptors
+
+	return genres
+
+
+def tag_album(album_item: pylast.Album, tags: list[str]):
+	# Add the tags
+	if not DRY_RUN:
+		if REMOVE_TAGS:
+			album_item.remove_tags(tags=album_item.get_tags())
+
+		if AUTO_TAGGED_TAG:
+			album_item.add_tag("auto-tagged")
+
+		album_item.add_tags(tags=tags)
+
+
+def get_albums() -> list[pylast.TopItem]:
+	if TAG_THIS_WEEK:
+		return user.get_weekly_album_charts(user.get_weekly_chart_dates()[0])
+	else:
+		return user.get_top_albums(limit=ALBUM_LIMIT)
+
+
+def already_tagged(album_item: pylast.Album) -> bool:
+	# Skips albums with the auto-tagged tag
+	if SKIP_AUTO_TAGGED:
+		for tag in album_item.get_tags():
+			if "auto-tagged" in tag.name:
+				return True
+
+	return False
+
+
+def message(album_item: pylast.Album, index: int, total: int, prefix="", suffix="", clean_name=False):
+	max_length = 160
+	bar_length = 60
+
+	album_title = clean_album_title(album_item.title) if clean_name else album_item.title
+
+	# Fancy progress bar stuff
+	filled_len = int(round(bar_length * index / float(total)))
+	percents = round(100.0 * index / float(total), 1)
+	bar = "█" * filled_len + "-" * (bar_length - filled_len)
+
+	justify = " " * ((max_length - bar_length) - (
+			len(album_title) + len(str(album_item.artist)) + len(prefix) + len(suffix)))
+	print("%s%s by %s %s%s [%s] %s%s  %s/%s  ETA: %s" % (prefix, album_title, album_item.artist, suffix, justify, bar,
+														 percents, '%', index, total, str(datetime.timedelta(
+		seconds=(((time.time() - start) / (index + 1)) * (total - index))))))
+
+
+def clean_album_title(title: str) -> str:
+	for common_term in ["original", "version", "remastered", "mixtape", "deluxe", "edition", "extended", "the remaster",
+						"remaster", "expanded", "bonus track", "complete"]:
+		title = title.lower().replace(common_term, "")
+
+	# Removes text within brackets
+	return re.sub("\\(.*?\\)", "", title).strip()
+
+
+def clean_tags(items: list[str]) -> list[str]:
 	tags = []
 
 	# Removes all unsupported characters for lastfm tags (keeps alphanumeric characters, space and dash)
 	for item in items:
-		tags.append(re.sub(r"[^0-9A-Za-z -]+", '', item.replace("&", "n")).strip())
+		tags.append(re.sub(r"[^0-9A-Za-z -]+", "", item.replace("&", "n")).strip())
 
 	return tags
 
@@ -21,7 +99,7 @@ if __name__ == '__main__':
 	# Whether existing tags should be removed or not
 	REMOVE_TAGS = True
 
-	# Whether RYM descriptors should be used as tags
+	# Whether RYM descriptors should be used as tags, disable if unsure what descriptors are
 	USE_DESCRIPTORS = True
 
 	# Whether to add the auto-tagged tag (needed to skip already tagged albums, see SKIP_AUTO_TAGGED)
@@ -41,8 +119,11 @@ if __name__ == '__main__':
 	# to continue from that point
 	SKIP_INDEX = 0
 
+	# Whether to print the genres and descriptors from RYM
+	PRINT_TAGS = True
+
 	# Whether it should actually write the tags or just display them, if True no tags will be changed
-	DRY_RUN = False
+	DRY_RUN = True
 
 	# Reads in the lastfm credentials
 	load_dotenv()
@@ -65,61 +146,35 @@ if __name__ == '__main__':
 	# Get the user
 	user = lastfm_network.get_authenticated_user()
 
-	if TAG_THIS_WEEK:
-		albums = user.get_weekly_album_charts(user.get_weekly_chart_dates()[0])
-	else:
-		albums = user.get_top_albums(limit=ALBUM_LIMIT)
+	albums = get_albums()
+	album_count = len(albums)
 
 	print(
 		f"CONFIG: REMOVE_TAGS: {REMOVE_TAGS}, USE_DESCRIPTORS: {USE_DESCRIPTORS}, AUTO_TAGGED_TAG: {AUTO_TAGGED_TAG}, "
 		f"SKIP_AUTO_TAGGED: {SKIP_AUTO_TAGGED}, TAG_THIS_WEEK: {TAG_THIS_WEEK}, ALBUM_LIMIT: {ALBUM_LIMIT}, SKIP_INDEX:"
-		f" {SKIP_INDEX}, DRY_RUN: {DRY_RUN}, user: {lastfm_network.get_authenticated_user().name}"
+		f" {SKIP_INDEX}, DRY_RUN: {DRY_RUN}, album_count: {album_count}, user: "
+		f"{lastfm_network.get_authenticated_user().name}\n"
 	)
 
-	index = 1
+	start = time.time()
 
-	for album in albums:
-		if index >= SKIP_INDEX:
-			already_tagged = False
-
-			# Skips albums with the auto-tagged tag
-			if SKIP_AUTO_TAGGED:
-				for tag in album.item.get_tags():
-					if "auto-tagged" in tag.name:
-						already_tagged = True
-						break
-
-			if already_tagged:
-				print(f"Skipping: {album.item.title} by {album.item.artist} ({index}) - already tagged\n")
+	for i, album in enumerate(albums):
+		if i + 1 >= SKIP_INDEX:
+			if already_tagged(album.item):
+				message(album_item=album.item, index=i, total=album_count, prefix="Skipping: ", suffix="- already tagged")
 			else:
+				found = True
 				try:
-					print(f"{album.item.title} by {album.item.artist} ({index}):")
-
-					# Retrieve the album from RYM
-					rym_album = rym_network.get_album_infos(name=f"{album.item.artist} - {album.item.title}")
-
-					# Extract and process the genres and descriptors
-					genres = clean(rym_album.get("Genres").replace("\n", ", ").split(", "))
-					print(f"Genres:      {genres}")
-
-					descriptors = clean(
-						rym_album.get("Descriptors").replace("\n", ", ").split(", ")) if USE_DESCRIPTORS else None
-					print(f"Descriptors: {descriptors}\n")
-
-					# Add the tags
-					if not DRY_RUN:
-						if REMOVE_TAGS:
-							album.item.remove_tags(tags=album.item.get_tags())
-
-						if USE_DESCRIPTORS:
-							album.item.add_tags(tags=descriptors)
-
-						if AUTO_TAGGED_TAG:
-							album.item.add_tag("auto-tagged")
-
-						album.item.add_tags(tags=genres)
-
+					album_tags = get_tags(album.item, False)
 				except (IndexError, AttributeError, TypeError) as e:
-					print(f"Unable to find {album.item.title} by {album.item.artist} ({index}) on RYM\n")
+					print(
+						f"Unable to find {album.item.title} by {album.item.artist} on RYM - trying to sanitize the album title...")
+					try:
+						album_tags = get_tags(album.item, True)
+					except (IndexError, AttributeError, TypeError) as e:
+						print(f"Unable to find {clean_album_title(album.item.title)} by {album.item.artist} on RYM")
+						found = False
+
+				if found:
+					tag_album(album.item, album_tags)
 				time.sleep(2.5)
-		index += 1
